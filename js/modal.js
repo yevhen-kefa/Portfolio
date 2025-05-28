a92908f4ce3afa247e393109aca5c7df7031fdfd
yevhen-kefa <yevhenkefa@gmail.com>
2025-05-28 11:47:28 +0200

    Update projet

--- js/modal.js
+++ js/modal.js
@@ -3,7 +3,147 @@ document.addEventListener('DOMContentLoaded', () => {
   const popup = document.getElementById('popup-container');
   const popupContent = document.getElementById('popup-content');
 
+  const imageViewer = document.getElementById('image-viewer');
+const fullImage = document.getElementById('full-image');
+const closeViewer = document.getElementById('close-viewer');
+
+// Делегуємо натискання на зображення в модальному вікні
+popupContent.addEventListener('click', (e) => {
+    if (e.target.tagName === 'IMG') {
+        fullImage.src = e.target.src;
+        imageViewer.style.display = 'flex';
+    }
+});
+
+// Закрити перегляд при кліку на "X"
+closeViewer.addEventListener('click', () => {
+    imageViewer.style.display = 'none';
+});
+
+
   const descriptions = {
+    kyoto: `
+          <div class="popup-content">
+              <header class="modal__header" onclick="togglePopup('kyoto')">
+                  <h2  class="h2 form-title">Kyoto</h2>
+              </header>
+              <div class="modal__content">
+                  <div class="modal__blocks">
+                      <div class="modal__block">
+                          <img src="img/kyoto.png" alt="kyoto">
+                          <p>Dans ce projet, nous avons développé un jeu en Python Qix en équipes de deux. Le but de ce projet était d'apprendre à travailler avec des objets en Python en utilisant la bibliothèque fltk (une bibliothèque conçue pour simplifier le travail avec les fenêtres et les objets), et à la fin nous devions présenter le code de travail du jeu à un comité d'enseignants. </p>
+                      </div>
+                      <div class="modal__block">
+                          <img src="img/projects/kyoto_1.png" alt="kyoto">
+                          <p>Qix est un jeu d'arcade dans lequel les joueurs capturent des parties du plateau de jeu pour créer des zones fermées. Les joueurs contrôlent un « marqueur » qui se déplace sur les bords du terrain et peut y pénétrer en laissant une trace. La tâche du joueur est de capturer un certain pourcentage du terrain tout en évitant le contact avec les ennemis et les obstacles. </p>
+                      </div>
+                      <div class="modal__block">
+                          <p>En conséquence, après plusieurs mois de travail, notre équipe a développé un jeu avec une logique fonctionnelle, des animations fluides et des contrôles réactifs. Au cours du processus de développement, j'étais responsable du mouvement du personnage, de la possibilité de peindre la zone de jeu, du mouvement des ennemis et de l'interaction avec eux. Le deuxième membre de notre équipe a travaillé sur les graphiques, les animations et a contribué à développer l'interaction avec les ennemis.</p>
+                          <img src="img/projects/kyoto_2.png" alt="kyoto">
+                      </div>
+                  </div>
+              </div>
+              <h3 class="h3 skills-title"><a class="liens" href="https://github.com/yevhen-kefa/Qix#">Voir sur GitHub</a> </h3>
+              <section class="skill">
+                  <h3 class="h3 skills-title">Utilisé: </h3>
+                  <ul class="skills-list">
+                       <li class="skills-item">
+                        <img src="img/html.png" alt="Html" />
+                        <h5 class="h5">Html5</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/css.png" alt="CSS" />
+                        <h5 class="h5">CSS3</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/javascript.png" alt="JavaScript" />
+                        <h5 class="h5">JavaScript</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/php.png" alt="php" />
+                        <h5 class="h5">Php</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/sql.png" alt="sql" />
+                        <h5 class="h5">Sql</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/MySQL.png" alt="sql" />
+                        <h5 class="h5">MySQL</h5>
+                      </li>
+                       <li class="skills-item">
+                        <img src="img/figma.png" alt="Figma" />
+                        <h5 class="h5">Figma</h5>
+                      </li>
+                    </ul>
+              </section>
+          </div>
+      `,
+      silveterna: `
+          <div class="popup-content">
+              <header class="modal__header" onclick="togglePopup('silveterna')">
+                  <h2  class="h2 form-title">SilvEterna</h2>
+              </header>
+              <div class="modal__content">
+                  <div class="modal__blocks">
+                      <div class="modal__block">
+                          <img src="img/silveterna.png" alt="v">
+                          <p>Dans ce projet, nous avons développé un jeu en Python Qix en équipes de deux. Le but de ce projet était d'apprendre à travailler avec des objets en Python en utilisant la bibliothèque fltk (une bibliothèque conçue pour simplifier le travail avec les fenêtres et les objets), et à la fin nous devions présenter le code de travail du jeu à un comité d'enseignants. </p>
+                      </div>
+                      <div class="modal__block">
+                          <img src="img/projects/silveterna_1.png" alt="silveterna">
+                          <p>Qix est un jeu d'arcade dans lequel les joueurs capturent des parties du plateau de jeu pour créer des zones fermées. Les joueurs contrôlent un « marqueur » qui se déplace sur les bords du terrain et peut y pénétrer en laissant une trace. La tâche du joueur est de capturer un certain pourcentage du terrain tout en évitant le contact avec les ennemis et les obstacles. </p>
+                      </div>
+                      <div class="modal__block">
+                          <p>En conséquence, après plusieurs mois de travail, notre équipe a développé un jeu avec une logique fonctionnelle, des animations fluides et des contrôles réactifs. Au cours du processus de développement, j'étais responsable du mouvement du personnage, de la possibilité de peindre la zone de jeu, du mouvement des ennemis et de l'interaction avec eux. Le deuxième membre de notre équipe a travaillé sur les graphiques, les animations et a contribué à développer l'interaction avec les ennemis.</p>
+                          <img src="img/projects/silveterna_2.png" alt="silveterna">
+                      </div>
+                      <div class="modal__block">
+                          <img src="img/projects/silveterna_3.png" alt="silveterna">
+                          <img src="img/projects/silveterna_4.png" alt="silveterna">
+                      </div>
+                  </div>
+              </div>
+              <h3 class="h3 skills-title"><a class="liens" href="https://github.com/yevhen-kefa/Qix#">Voir sur GitHub</a> </h3>
+              <section class="skill">
+                  <h3 class="h3 skills-title">Utilisé: </h3>
+                  <ul class="skills-list">
+                       <li class="skills-item">
+                        <img src="img/html.png" alt="Html" />
+                        <h5 class="h5">Html5</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/css.png" alt="CSS" />
+                        <h5 class="h5">CSS3</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/javascript.png" alt="JavaScript" />
+                        <h5 class="h5">JavaScript</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/php.png" alt="php" />
+                        <h5 class="h5">Php</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/sql.png" alt="sql" />
+                        <h5 class="h5">Sql</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/MySQL.png" alt="sql" />
+                        <h5 class="h5">MySQL</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/PostgreSQL.png" alt="sql" />
+                        <h5 class="h5">PostgreSQL</h5>
+                      </li>
+                       <li class="skills-item">
+                        <img src="img/figma.png" alt="Figma" />
+                        <h5 class="h5">Figma</h5>
+                      </li>
+                    </ul>
+              </section>
+          </div>
+      `,
     unesco: `
         <div class="popup-content">
           <header class="modal__header" onclick="togglePopup('unesco')">
@@ -61,6 +201,38 @@ document.addEventListener('DOMContentLoaded', () => {
           </section>
         </div>
       `,
+      tetris: `
+          <div class="popup-content">
+              <header class="modal__header" onclick="togglePopup('tetris')">
+                  <h2  class="h2 form-title">Tetris</h2>
+              </header>
+              <div class="modal__content">
+                  <div class="modal__blocks">
+                      <div class="modal__block">
+                          <img src="img/tetris.png" alt="tetris">
+                          <p>Dans ce projet, nous avons développé un jeu en Python Qix en équipes de deux. Le but de ce projet était d'apprendre à travailler avec des objets en Python en utilisant la bibliothèque fltk (une bibliothèque conçue pour simplifier le travail avec les fenêtres et les objets), et à la fin nous devions présenter le code de travail du jeu à un comité d'enseignants. </p>
+                      </div>
+                      <div class="modal__block">
+                          <img src="img/projects/tetris_1.png" alt="tetris">
+                          <p>Qix est un jeu d'arcade dans lequel les joueurs capturent des parties du plateau de jeu pour créer des zones fermées. Les joueurs contrôlent un « marqueur » qui se déplace sur les bords du terrain et peut y pénétrer en laissant une trace. La tâche du joueur est de capturer un certain pourcentage du terrain tout en évitant le contact avec les ennemis et les obstacles. </p>
+                      </div>
+                      <div class="modal__block">
+                          <p>En conséquence, après plusieurs mois de travail, notre équipe a développé un jeu avec une logique fonctionnelle, des animations fluides et des contrôles réactifs. Au cours du processus de développement, j'étais responsable du mouvement du personnage, de la possibilité de peindre la zone de jeu, du mouvement des ennemis et de l'interaction avec eux. Le deuxième membre de notre équipe a travaillé sur les graphiques, les animations et a contribué à développer l'interaction avec les ennemis.</p>
+                          <img src="img/projects/tetris_2.png" alt="tetris">
+                      </div>
+                  </div>
+              </div>
+              <h3 class="h3 skills-title"><a class="liens" href="https://github.com/yevhen-kefa/Qix#">Voir sur GitHub</a> </h3>
+              <section class="skill">
+                  <h3 class="h3 skills-title">Utilisé: </h3>
+                  <ul class="skills-list">
+                      <li class="skills-item">
+                          <img src="img/python.png" alt="Python" />
+                          <h5 class="h5">Python</h5>
+                      </li>
+              </section>
+          </div>
+      `,
       qix: `
           <div class="popup-content">
               <header class="modal__header" onclick="togglePopup('qix')">
@@ -82,6 +254,7 @@ document.addEventListener('DOMContentLoaded', () => {
                       </div>
                   </div>
               </div>
+              <h3 class="h3 skills-title"><a class="liens" href="https://github.com/yevhen-kefa/Qix#">Voir sur GitHub</a> </h3>
               <section class="skill">
                   <h3 class="h3 skills-title">Utilisé: </h3>
                   <ul class="skills-list">
@@ -89,6 +262,71 @@ document.addEventListener('DOMContentLoaded', () => {
                           <img src="img/python.png" alt="Python" />
                           <h5 class="h5">Python</h5>
                       </li>
+              </section>
+          </div>
+      `,
+      primeur_passions: `
+          <div class="popup-content">
+              <header class="modal__header" onclick="togglePopup('primeur_passions')">
+                  <h2  class="h2 form-title">Qix</h2>
+              </header>
+              <div class="modal__content">
+                  <div class="modal__blocks">
+                      <div class="modal__block">
+                          <img src="img/primeurpassions.png" alt="primeur_passions">
+                          <p>Dans ce projet, nous avons développé un jeu en Python Qix en équipes de deux. Le but de ce projet était d'apprendre à travailler avec des objets en Python en utilisant la bibliothèque fltk (une bibliothèque conçue pour simplifier le travail avec les fenêtres et les objets), et à la fin nous devions présenter le code de travail du jeu à un comité d'enseignants. </p>
+                      </div>
+                      <div class="modal__block">
+                          <img src="img/projects/primeurpassions_1.png" alt="primeur_passions">
+                          <p>Qix est un jeu d'arcade dans lequel les joueurs capturent des parties du plateau de jeu pour créer des zones fermées. Les joueurs contrôlent un « marqueur » qui se déplace sur les bords du terrain et peut y pénétrer en laissant une trace. La tâche du joueur est de capturer un certain pourcentage du terrain tout en évitant le contact avec les ennemis et les obstacles. </p>
+                      </div>
+                      <div class="modal__block">
+                          <p>En conséquence, après plusieurs mois de travail, notre équipe a développé un jeu avec une logique fonctionnelle, des animations fluides et des contrôles réactifs. Au cours du processus de développement, j'étais responsable du mouvement du personnage, de la possibilité de peindre la zone de jeu, du mouvement des ennemis et de l'interaction avec eux. Le deuxième membre de notre équipe a travaillé sur les graphiques, les animations et a contribué à développer l'interaction avec les ennemis.</p>
+                          <img src="img/projects/primeurpassions_2.png" alt="primeur_passions">
+                      </div>
+                      <div class="modal__block">
+                          <img src="img/projects/primeurpassions_3.png" alt="primeur_passions">
+                          <img src="img/projects/primeurpassions_4.png" alt="primeur_passions">
+                      </div>
+                  </div>
+              </div>
+              <h3 class="h3 skills-title"><a class="liens" href="https://github.com/yevhen-kefa/Qix#">Voir sur GitHub</a> </h3>
+              <section class="skill">
+                  <h3 class="h3 skills-title">Utilisé: </h3>
+                  <ul class="skills-list">
+                       <li class="skills-item">
+                        <img src="img/html.png" alt="Html" />
+                        <h5 class="h5">Html5</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/css.png" alt="CSS" />
+                        <h5 class="h5">CSS3</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/javascript.png" alt="JavaScript" />
+                        <h5 class="h5">JavaScript</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/php.png" alt="php" />
+                        <h5 class="h5">Php</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/sql.png" alt="sql" />
+                        <h5 class="h5">Sql</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/MySQL.png" alt="sql" />
+                        <h5 class="h5">MySQL</h5>
+                      </li>
+                      <li class="skills-item">
+                        <img src="img/PostgreSQL.png" alt="sql" />
+                        <h5 class="h5">PostgreSQL</h5>
+                      </li>
+                       <li class="skills-item">
+                        <img src="img/figma.png" alt="Figma" />
+                        <h5 class="h5">Figma</h5>
+                      </li>
+                    </ul>
               </section>
           </div>
       `,
@@ -115,6 +353,7 @@ document.addEventListener('DOMContentLoaded', () => {
                  
               </div>
           </div>
+          <h3 class="h3 skills-title"><a class="liens" href="#"> Cimetiere</a> </h3>
           <section class="skill">
                   <h3 class="h3 skills-title">Utilisé: </h3>
                   <ul class="skills-list">
@@ -137,6 +376,29 @@ document.addEventListener('DOMContentLoaded', () => {
               </section>
         </div>
       `,
+      yams: `
+          <div class="popup-content">
+              <header class="modal__header" onclick="togglePopup('yams')">
+                  <h2  class="h2 form-title">Yams</h2>
+              </header>
+              <div class="modal__content">
+                  <div class="modal__blocks">
+                      <div class="modal__block">
+                          <p>En cours de développement</p>
+                      </div>
+                  </div>
+              </div>
+              <h3 class="h3 skills-title"><a class="liens" href="https://github.com/yevhen-kefa/Qix#">Voir sur GitHub</a> </h3>
+              <section class="skill">
+                  <h3 class="h3 skills-title">Utilisé: </h3>
+                  <ul class="skills-list">
+                      <li class="skills-item">
+                          <img src="img/java.png" alt="Java" />
+                          <h5 class="h5">Java</h5>
+                      </li>
+              </section>
+          </div>
+      `,
       codex: `
         <div class="popup-content">
           <header class="modal__header" onclick="togglePopup('codex')">
@@ -158,6 +420,7 @@ document.addEventListener('DOMContentLoaded', () => {
                   </div>
               </div>
           </div>
+          <h3 class="h3 skills-title"><a class="liens" href="#"> Voire le code de Codex Naturalist</a> </h3>
           <section class="skill">
           <h3 class="h3 skills-title">Utilisé: </h3>
           <ul class="skills-list">
